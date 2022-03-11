9a58911d176a8a32e43e5437327fab731b4c0ea8
Darren Kulback <dkulback@gmail.com>
2022-03-11 16:37:29 -0700

    all http connections

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -92,7 +92,7 @@ VCR.configure do |config|
   }
 
   config.configure_rspec_metadata!
-  config.allow_http_connections_when_no_cassette = false
+  config.allow_http_connections_when_no_cassette = true
   config.filter_sensitive_data('fake_api_key') { ENV['weather_api_key'] }
   config.filter_sensitive_data('fake_key') { ENV['map_api_key'] }
   config.filter_sensitive_data('fake_api_key_2') { ENV['photo_api_key'] }
