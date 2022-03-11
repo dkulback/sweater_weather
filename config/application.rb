1779e86fe69082c914be39893a050133d925be89
Darren Kulback <dkulback@gmail.com>
2022-03-11 16:59:07 -0700

    update time zone

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,6 @@ module SweaterWeather
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options
+    config.time_zone = 'Mountain Time (US & Canada)'
   end
 end
