b0429e5f040b7434d5019ba3206209bbdda6f07f
Darren Kulback <dkulback@gmail.com>
2022-03-13 17:22:21 -0600

    move file for user mailer to find

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::UsersController < ApplicationController
     user = User.create!(email: params[:email], password: params[:password],
                         password_confirmation: params[:password_confirmation])
 
+    user.update(api_key: ApiKey.generator)
     WelcomeUserEmailJob.perform_async(user.email)
 
     render json: UsersSerializer.user(user), status: :created
