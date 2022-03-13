6872d5fed6bc61d35e30267658726c4b3533240f
Darren Kulback <dkulback@gmail.com>
2022-03-13 15:31:43 -0600

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ApplicationMailer
   default from: 'dkulback@gmail.com'
 
   def welcome_email(email)
+    require 'pry'
+    binding.pry
     @url = 'https://shielded-atoll-16827.herokuapp.com'
     mail(to: email, subject: 'Welcome to My Sidekiq Job')
   end
