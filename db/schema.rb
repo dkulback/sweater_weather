e35a027735f8083fb7c3c2376c0434d252350cca
Darren Kulback <dkulback@gmail.com>
2022-10-26 16:11:50 -0600

    Merge db schema into branch
    Rubocop fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_24_151106) do
-
+ActiveRecord::Schema.define(version: 20_220_424_151_106) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "api_key"
-    t.index ["api_key"], name: "index_users_on_api_key", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'email'
+    t.string 'password_digest'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'api_key'
+    t.index ['api_key'], name: 'index_users_on_api_key', unique: true
   end
-
 end
