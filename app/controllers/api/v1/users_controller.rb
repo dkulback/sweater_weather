9434fdd521c26acd622017b5a01f11a3e7f60783
Darren Kulback <dkulback@gmail.com>
2022-10-27 17:39:56 -0600

    Add user cell phone request

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+  wrap_parameters :user, include: %i[email password cell password password_confirmation]
   def create
-    user = User.create!(email: params[:email], password: params[:password],
-                        password_confirmation: params[:password_confirmation])
-    render json: UsersSerializer.user(user), status: :created
+    user = User.create!(user_params)
+    if user.save
+      session[:user_id] = user.id
+
+      render json: UsersSerializer.user(user), status: :created
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :email)
   end
 end
