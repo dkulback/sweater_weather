3bbbb289cf7452d5585f5de9bbe536a7a0e9203d
Darren Kulback <dkulback@gmail.com>
2022-10-27 18:03:54 -0600

    Add db user auth schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_424_151_106) do
+ActiveRecord::Schema.define(version: 2022_04_24_151106) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
